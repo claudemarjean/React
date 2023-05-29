5e5446a78d1eab66ea0151250b3258d96ef6c086
claudemarjean <marjean.8888@gmail.com>
2023-05-29 22:32:13 +0300

    incrementer un state, vidéo: 004 Exercice_incrementer un state dans react 28:00

--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -3,20 +3,35 @@ import Car from './Cars';
 
 class Mycars extends Component{
 
-    noCopy = () =>{
-        alert('merci de ne pas copier le texte');
+    state = {
+        voitures : [
+            {name: 'Ford', color : 'red', year : 2000},
+            {name: 'Mercedes', color : 'black', year : 2010},
+            {name: 'Peugeot', color : 'green', year : 2018}   
+        ]
     }
 
+    addTenYears = ()=>{
+        const updatedState = this.state.voitures.map((param)=>{
+            return param.year -= 10
+        })
+        this.setState({
+            updatedState
+        })
+    }
 
     render(){
+        const year = new Date().getFullYear();
+
         return (
             <div>
                 <h1 >{this.props.title}</h1>
 
-                <p onCopy={this.noCopy}>La classe était considérée comme un “State Component” avant l\’arrivé des Hooks dans React.</p>
-                <Car color="red"></Car>
-                <Car color="black">Mercedes</Car>
-                <Car color="green">Peugeot</Car>
+                <button onClick={this.addTenYears}> +10 ans</button>
+
+                 <Car color={this.state.voitures[0].color} year={year - this.state.voitures[0].year + ' ans'}>{this.state.voitures[0].name}</Car>
+                 <Car color={this.state.voitures[1].color} year={year - this.state.voitures[1].year + ' ans'}>{this.state.voitures[1].name}</Car>
+                 <Car color={this.state.voitures[2].color} year={year - this.state.voitures[2].year + ' ans'}>{this.state.voitures[2].name}</Car>
             </div>
         );
     }
