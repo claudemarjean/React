8af90c5218bb096181443c1b2feeff1e8d286b5d
claudemarjean <marjean.8888@gmail.com>
2023-05-29 19:17:16 +0300

    evenement et changement de state 1

--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -2,11 +2,27 @@ import React, {Component} from 'react';
 import Car from './Cars';
 
 class Mycars extends Component{
+
+    noCopy = () =>{
+        alert('merci de ne pas copier le texte');
+    }
+
+    addStyle = (e) =>{
+
+        if(e.target.classList.contains('styled')){
+            e.target.classList.remove('styled');
+        }
+        else{
+            e.target.classList.add('styled');
+        }
+    }
+
     render(){
         return (
             <div>
-                <h1>{this.props.title}</h1>
+                <h1 onMouseOver={this.addStyle}>{this.props.title}</h1>
 
+                <p onCopy={this.noCopy}>La classe était considérée comme un “State Component” avant l\’arrivé des Hooks dans React.</p>
                 <Car color="red"></Car>
                 <Car color="black">Mercedes</Car>
                 <Car color="green">Peugeot</Car>
