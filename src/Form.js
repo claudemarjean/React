0983c6840efcb433b11e788100f1364e2f6d6f2d
claudemarjean <marjean.8888@gmail.com>
2023-06-03 23:50:13 +0300

    formulaire 003-11 07:00

--- src/Form.js
+++ src/Form.js
@@ -2,12 +2,25 @@ import React,{Component} from "react";
 import Car from "./Car";
 
 class Form extends Component{
+    state={
+        username: '',
+    }
+
+    handlePseudo = e =>{
+        this.setState({
+            username: e.target.value
+        })
+    }
+
     render(){
         return(
             <div>
                 <Car color="red" height="400"/>
                 <h1>Commentaire</h1>
-                Formulaire
+                <form>
+                    <label>Pseudo</label>
+                    <input type="text"  value={this.state.username} onChange={this.handlePseudo}/>
+                </form>
             </div>
         )
     }
