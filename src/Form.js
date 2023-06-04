6a1f6443c617d66be35a3b590bf87084ea8bd90b
claudemarjean <marjean.8888@gmail.com>
2023-06-04 12:49:52 +0300

    style inline

--- src/Form.js
+++ src/Form.js
@@ -1,67 +1,19 @@
 import React,{Component} from "react";
-import Car from "./Car";
+
+const titreRouge = {
+    fontSize: '50px', 
+    color : "red"
+}
 
 class Form extends Component{
-    state={
-        username: '',
-        color:'',
-        colors: ["", "red","blue","green","black","pink"],
-        comment: ''
-    }
-
-    handlePseudo = e =>{
-        this.setState({
-            username: e.target.value
-        })
-    }
-
-    handleCouleur = event =>{
-        this.setState({
-            color: event.target.value
-        })
-    }
-
-    handleComment = e =>{
-        this.setState({
-            comment: e.target.value
-        })
-    }
-
-    handleSubmitForm = e =>{
-        e.preventDefault();
-        console.log(`Username: ${this.state.username} Couleur: ${this.state.color} Commentaire: ${this.state.comment}`)
-    }
-
-
     render(){
         return(
             <div>
-                <Car color={this.state.color} height="400"/>
-                <h1>Commentaire</h1>
-                <form onSubmit={this.handleSubmitForm}>
-                    <div>
-                        <label>Pseudo</label>
-                        <input type="text"  value={this.state.username} onChange={this.handlePseudo}/>
-                    </div>
-                    <div>
-                        <label>Couleur</label>
-                        <select value={this.state.color} onChange={this.handleCouleur}>
-                            {
-                                this.state.colors.map((color, index) =>{
-                                    return <option key={index} value={color} >{color}</option>
-                                })
-                            }
-                        </select>
-                    </div>
-                    <div>
-                        <label> Commentaire </label>
-                        <textarea value={this.state.comment} onChange={this.handleComment}></textarea>
-                    </div>
-                    <button> Valider</button>
-                </form>
+                <h1 style={titreRouge}>Commentaire</h1>
+                <button>Valider</button>
             </div>
         )
     }
 }
 
-export default Form; 
+export default Form;
