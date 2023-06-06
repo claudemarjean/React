2b464d8c3c144ecf544f5bb68a07119d18695c5d
claudemarjean <marjean.8888@gmail.com>
2023-06-06 06:47:03 +0300

    mounting phase

---
+++ src/LifeCycle.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class LifeCycle extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        name: 'Toto',
+        step: 1
+      }
+
+      console.log(`Etape ${this.state.step} :je suis dans le contructor()`);
+    }
+
+    componentDidMount() { 
+        console.log(`Etape ${this.state.step} :je suis dans le componentDidMount()`)
+     }
+
+    render() {
+        console.log(`Etape ${this.state.step} :je suis dans le render()`);
+        return (
+            <div>
+                {console.log(`Etape ${this.state.step} :je suis dans le maj du DOM`)}
+                <p>chargement: {this.state.step}</p>
+                <p>Nom: {this.state.name}</p>
+            </div>
+        )
+    }
+}
+
+export default LifeCycle
