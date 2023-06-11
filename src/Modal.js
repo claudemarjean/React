05ea5e8d47a83b032a9e0f177ce312521614d9db
claudemarjean <marjean.8888@gmail.com>
2023-06-11 11:09:45 +0300

    création de l'élément de DOM à la volet et suppression de l'élément dans componentWillUnmount

--- src/Modal.js
+++ src/Modal.js
@@ -2,6 +2,17 @@ import React,{Component} from "react";
 import ReactDOM from 'react-dom';
 
 class Modal extends Component{
+    constructor(props) {
+      super(props)
+      this.popUpContainer = document.createElement('div');
+
+      document.body.appendChild(this.popUpContainer);
+    }
+
+    componentWillUnmount() {
+        document.body.removeChild(this.popUpContainer);
+     }
+
     render(){
         return ReactDOM.createPortal(
             <div className="modal" onClick={this.props.close}>
@@ -14,7 +25,7 @@ class Modal extends Component{
                     </button>
                 </div>
             </div>,
-            document.getElementById('second-root')
+            this.popUpContainer
         )
     }
 }
