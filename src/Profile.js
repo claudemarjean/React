36b6575186db794fe45d394b9984f6db00c2192e
claudemarjean <marjean.8888@gmail.com>
2023-06-21 22:00:16 +0300

    context 2 05-013 finish

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,7 @@
 import React,{Component} from "react";
 import naruto from './naruto.png';
 import ProfileData from "./components/ProfileData";
+import { MyContext } from "./components/MyContext";
 
 const styleImage = {
     width: '30%'
@@ -8,14 +9,22 @@ const styleImage = {
 
 class Profile extends Component {
     render(){
+
+        let value = this.context;
+        console.log(value)
+
         return(
             <div className="container">
                 <h1>{this.props.info.name}</h1>
+                <p>Age: {value.age} ans</p>
                 <img src={naruto} alt="Naruto" className="img-trumbail mb-3" style={styleImage}/>
                 <ProfileData welcome={this.props.info}/>
             </div>
         )
     }
 }
+
+Profile.contextType = MyContext;
+console.log(Profile.contextType)
 
 export default Profile
