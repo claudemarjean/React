f3ed5c0b1effbdfb0fa403e6f163106981b259fe
claudemarjean <marjean.8888@gmail.com>
2023-07-03 23:18:07 +0300

    useReducer compliqué 06-011 finish🧪🧪

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,13 +1,20 @@
 import React,{useReducer} from 'react'
 
-const initialState = 0;
+const initialState = {
+    countOne: 0 ,
+    countTwo : 0
+};
 
 const reducer = (state, action) =>{
-    switch(action){
+    switch(action.type){
         case 'increment':
-            return state + 1
+            return {...state, countOne: state.countOne + action.value}
         case 'decrement':
-            return state -1
+            return {...state, countOne: state.countOne - action.value}
+        case 'increment2':
+            return {...state, countTwo: state.countTwo + action.value}
+        case 'decrement2':
+            return {...state, countTwo: state.countTwo - action.value}
         case 'reinitialiser':
             return initialState
         default:
@@ -19,11 +26,30 @@ function Count() {
 
     const[count, dispatch] = useReducer(reducer,initialState);
   return (
-    <div>
-        <h1>{count}</h1>
-        <button className='btn btn-success m-3' onClick={()=>dispatch('increment')}>+</button>
-        <button className='btn btn-danger m-3' onClick={()=>dispatch('decrement')}>-</button>
-        <button className='btn btn-primary m-3' onClick={()=>dispatch('reinitialiser')}>0</button>
+    <div className='row'>
+        <div className='col-6'>
+            <p className='h1'>{count.countOne}</p>
+            <button className='btn btn-success m-3' onClick={
+                ()=>dispatch({type: 'increment', value: 1})
+            }>+</button>
+            <button className='btn btn-danger m-3' onClick={
+                ()=>dispatch({type: 'decrement', value: 1})
+            }>-</button>
+        </div>
+        <div className='col-6'>
+            <p className='h1'>{count.countTwo}</p>
+            <button className='btn btn-success m-3' onClick={
+                ()=>dispatch({type: 'increment2', value: 10})
+            }>+</button>
+            <button className='btn btn-danger m-3' onClick={
+                ()=>dispatch({type: 'decrement2', value: 10})
+            }>-</button>
+        </div>
+        <div className='col'>
+            <button className='btn btn-primary m-3' onClick={
+                    ()=>dispatch({type: 'reinitialiser'})
+            }>0</button>
+        </div>
     </div>
   )
 }
