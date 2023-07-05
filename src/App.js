11963d742cd9a6fcb53721c404a1028217f7d7c9
claudemarjean <marjean.8888@gmail.com>
2023-07-05 22:29:03 +0300

    cas d'avant utilisation useMemo♻️♻️

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React,{useState, useEffect} from 'react'
 import './App.css';
 import Profile from './components/Profile';
+import axios from 'axios'
 
 function App() {
 
   const[count, setCount] = useState(1);
+  const[profile,setProfile] = useState({});
+  const[dark, setDark] = useState(false)
 
   useEffect(()=>{
-    effect
-  },[input]);
+    axios
+    .get(`https://jsonplaceholder.typicode.com/users/${count}`)
+    .then(response=>{
+      setProfile(response.data)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  },[count]);
+
+  const goDark = () =>{
+    setDark(!dark)
+
+    if(!dark){
+      document.body.classList.add("bg-secondary")
+    }
+    else(
+      document.body.classList.remove("bg-secondary")
+    )
+  }
+
+  const classBtnTheme = dark ? 'btn-light' : 'btn-dark';
+  const textBtnTheme = dark ? 'Rendre clair' : 'rendre sombre';
+
+  const isOverThen = () =>{
+    console.log("jesuis dans isOverThen")
+    return count > 10
+  }
 
   return (
     <div className='container'>
       <h1 className='text-center'>UseMemo()</h1>
+      {isOverThen() && <div className='alert alert-danger' role="alert">STOP!!!</div>}
       <button className="btn btn-info mb-3" onClick={()=>{setCount(count + 1)}}>Increment {count}</button>
-      <button className="btn btn-dark mb-3 float-end">Changer</button>
+      <button className={`btn ${classBtnTheme} mb-3 float-end`} onClick={goDark}>{textBtnTheme}</button>
 
-      <Profile/>
+      <Profile count={count} profile={profile}/>
     </div>
   )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
-function Profile() {
+function Profile({count, profile}) {
+    console.log("je suis dans la focntion profile");
   return (
     <div>
         <ul className='list-group'>
-            <li className='list-group-item'><p className='h2'>User ID:</p></li>
-            <li className='list-group-item'><strong>Nom:</strong></li>
-            <li className='list-group-item'><strong>Pseudo:</strong></li>
-            <li className='list-group-item'><strong>Email:</strong></li>
+            <li className='list-group-item'><p className='h2'>User ID:{count}</p></li>
+            <li className='list-group-item'><strong>Nom:{profile.name}</strong></li>
+            <li className='list-group-item'><strong>Pseudo:{profile.username}</strong></li>
+            <li className='list-group-item'><strong>Email:{profile.email}</strong></li>
         </ul>
     </div>
   )
 }
 
-export default Profile
+export default React.memo(Profile)
