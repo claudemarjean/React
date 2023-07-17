a8adbcaef00efadb0cca11c18f17d25f2ac075c6
claudemarjean <marjean.8888@gmail.com>
2023-07-17 19:02:31 +0300

    configuration de Firebase💥💥

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyB5MHP9EzoEvONVOdd2VgRrKkNiuW4NoqM",
+    authDomain: "marvel-quiz-b896b.firebaseapp.com",
+    projectId: "marvel-quiz-b896b",
+    storageBucket: "marvel-quiz-b896b.appspot.com",
+    messagingSenderId: "751455399000",
+    appId: "1:751455399000:web:31bc3e51bc11f15e352e2d"
+  };
+
+class Firebase{
+    constructor(){
+        app.initializeApp(config)
+    }
+}
+
+export default Firebase;
