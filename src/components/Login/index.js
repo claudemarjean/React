cfac27744849c773491989409c137bf9d8283dac
claudemarjean <marjean.8888@gmail.com>
2023-07-26 21:23:48 +0300

    07-012 08:40

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {Link } from 'react-router-dom';
 
 const Login = () => {
+
+   const[email, setEmail]= useState('');
+   const[password, setPassword]= useState('');
+   
+
   return (
     <div className='singUpLoginBox'>
         <div className='slContainer'> 
-            LOGIN
-        </div>
+              <div className='formBoxLeftLogin'>
+              
+              </div>
+              <div className='formBoxRightLogin'>
+                <div className='formContent'>
+                <h2>Connexion</h2>
+                  <form>
+                    <div className='inputBox'>
+                      <input onChange={e => setEmail(e.target.value)} value={email}  autoComplete='off' type="email" required />
+                      <label htmlFor='email'>Email</label>
+                    </div>
+                    <div className='inputBox'>
+                      <input onChange={e => setPassword(e.target.value)} value={password}  autoComplete='off' type="password" required />
+                      <label htmlFor='password'>Mot de passe</label>
+                    </div>
+                    
+                  </form>
+                  <div className='linkContainer'>
+                    <Link className='simpleLink' to='/signup'>Nouveau sur marvel Quiz? Inscrivez-vous maintenant</Link>
+                  </div>
+                </div>
+              </div>
+          </div>
     </div>
   )
 }
