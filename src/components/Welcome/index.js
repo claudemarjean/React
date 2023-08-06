637db09978a4cc335f830763154920d6e546c485
claudemarjean <marjean.8888@gmail.com>
2023-08-06 20:58:49 +0300

    gestion de session 07-015 finish

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{useState, Fragment, useContext, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FirebaseContext } from '../Firebase';
 import Logout from '../Logout'
 import Quiz from '../Quiz'
 
-const Welcome = () => {
-  return (
+const Welcome = props => {
+
+  const firebase = useContext(FirebaseContext);
+  const[userSession, setUserSession] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged(user=>{
+      user ? setUserSession(user) : navigate('/');
+    });
+
+    return () => {
+      listener()
+    }
+  }, [])
+  
+
+  return userSession === null ? (
+    <Fragment>
+      <div className='loader'></div>
+      <p>Loading</p>
+    </Fragment>
+  ):
+  (
     <div className='quiz-bg'>
         <div className='Container'>
             <Logout/>
