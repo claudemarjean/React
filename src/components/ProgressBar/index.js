3ad7b9021aaa7d6325c79fc2d6a23a36a2bcbe2c
claudemarjean <marjean.8888@gmail.com>
2023-09-03 21:45:34 +0300

    component Quiz, Levels, ProgressBar 07-019 finsh

---
+++ src/components/ProgressBar/index.js
@@ -0,0 +1,23 @@
+import React,{Fragment} from 'react'
+
+const ProgressBar = () => {
+  return (
+    <Fragment>
+        <div className='percentage'>
+            <div className='progressPercent'>
+                Question: 1/10
+            </div>
+            <div className='progressPercent'>
+                Progression: 10%
+            </div>
+        </div>
+        <div>
+            <div className='progressBarChange' style={{width: '50%'}}>
+                
+            </div>
+        </div>
+    </Fragment>
+  )
+}
+
+export default ProgressBar
