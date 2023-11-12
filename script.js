718270a74709c117057d3871306ffbc24e753e4c
AGENT M-109 <marjean.8888@gmail.com>
2023-11-12 18:04:23 +0300

    08-005 gestion de plusieurs action 🚀🚀🚀

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Définir le type d'action
 const BUY_PHONE = 'BUY_PHONE';
+const BUY_TABLET = 'BUY_TABLET';
 
 // Action creator
 let buyPhone = () => {
@@ -8,12 +9,18 @@ let buyPhone = () => {
     };
 }
 
+let buyTablet = () =>{
+    return {
+        type: BUY_TABLET
+    }
+}
+
 
 //Reducer
-//(prevState, action)=>newState
 
 const initialState = {
-    phones: 5
+    phones: 5,
+    tablet: 10
 }
 
 const reducer = (state = initialState, action) =>{
@@ -23,20 +30,33 @@ const reducer = (state = initialState, action) =>{
                 ...state,
                 phones: state.phones - 1
             }
-            break;
+        case BUY_TABLET:
+                return{
+                    ...state,
+                    tablet: state.tablet - 1
+                }
         default: return  state
     }
 }
 
+//créer le store
 const store = Redux.createStore(reducer);
+
+//récupérer la data du store
 const availablePhones = document.getElementById('count');
+const availableTablet = document.getElementById('countTab');
 availablePhones.innerHTML = store.getState().phones;
+availableTablet.innerHTML = store.getState().tablet;
 
 document.getElementById('buyPhone').addEventListener('click', function(){
     store.dispatch(buyPhone())
+})
+document.getElementById('buyTablet').addEventListener('click', function(){
+    store.dispatch(buyTablet())
 })
 
 
 store.subscribe(()=>{
     availablePhones.innerHTML = store.getState().phones;
+    availableTablet.innerHTML = store.getState().tablet;
 })
